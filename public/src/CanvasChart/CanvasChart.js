2ec27a19009e59fe537a51d7106db35f0a42ae8a
haandev <38D591x5++>
2021-12-03 22:29:47 +0300

    add: render method baby steps

--- public/src/CanvasChart/CanvasChart.js
+++ public/src/CanvasChart/CanvasChart.js
@@ -1,9 +1,23 @@
 var CanvasChart = (function () {
     function CanvasChart(config) {
         var series = config.series, options = config.options;
+        this.series = series;
+        this.options = options;
+        this.longestSerieLength = this.calculateLongestSerie(series);
+        this.canvasElement = document.createElement('canvas');
     }
+    CanvasChart.prototype.calculateLongestSerie = function (series) {
+        return Math.max.apply(Math, Object.values(series).map(function (currentSerie) { return currentSerie.length; }));
+    };
     CanvasChart.prototype.render = function (canvasContainerId) {
-        console.log("render method fired to create canvas in element : #".concat(canvasContainerId));
+        this.canvasContainer = document.getElementById(canvasContainerId);
+        if (!this.canvasContainer) {
+            console.log('A container element with the ID you specified was not found.');
+            return;
+        }
+        this.rendered = true;
+        this.canvasContainer.innerHTML = "";
+        this.canvasContainer.appendChild(this.canvasElement);
     };
     return CanvasChart;
 }());
--- public/src/CanvasChart/CanvasChart.ts
+++ public/src/CanvasChart/CanvasChart.ts
@@ -1,13 +1,40 @@
-import { ICanvasChart } from './types'
+import { CanvasChartOptions, CanvasChartSeries, ICanvasChart } from './types'
 
 class CanvasChart {
+  private series: CanvasChartSeries
+  private options: CanvasChartOptions
+  private longestSerieLength: number
+  private canvasElement: HTMLCanvasElement
+  private canvasContainer: HTMLElement | undefined
+  private rendered: boolean
+
   constructor(config: ICanvasChart) {
     const { series, options } = config
+    this.series = series
+    this.options = options
+    this.longestSerieLength = this.calculateLongestSerie(series)
+    this.canvasElement = document.createElement('canvas')
   }
-  render(canvasContainerId: string) {
-    console.log(
-      `render method fired to create canvas in element : #${canvasContainerId}`
+
+  calculateLongestSerie(series: CanvasChartSeries) {
+    return Math.max(
+      ...Object.values(series).map((currentSerie) => currentSerie.length)
     )
+  }
+
+  render(canvasContainerId: string) {
+    this.canvasContainer = document.getElementById(canvasContainerId)
+
+    if (!this.canvasContainer) {
+      console.log(
+        'A container element with the ID you specified was not found.'
+      )
+      return
+    }
+
+    this.rendered = true
+    this.canvasContainer.innerHTML=""
+    this.canvasContainer.appendChild(this.canvasElement)
   }
 }
 
